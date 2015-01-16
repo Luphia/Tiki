b278dd2a98953de0dc82afb6e6a5bf77d71be1f9
Luphia Chang <luphia@outlook.com>
2015-01-16 17:49:59 +0800

    Debug

--- bin/multi-test.js
+++ bin/multi-test.js
@@ -6,5 +6,5 @@ times = times > 0? times: 1;
 var cp = require('child_process');
 for(var i = 0; i < times; i++) {
 	cp.fork(__dirname + '/test.js');
-	if(i % 100 == 9) console.log("%d Bots lunched !!", i + 1);
+	if((i+1) % 100 == 0) console.log("%d Bots lunched !!", i + 1);
 }
