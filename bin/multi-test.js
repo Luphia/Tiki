405064eaf6bc49521df57e2109ff5f1caa3a41c5
Luphia Chang <luphia@outlook.com>
2015-01-16 17:37:29 +0800

    Show child process number per 100 bots

--- bin/multi-test.js
+++ bin/multi-test.js
@@ -6,4 +6,5 @@ times = times > 0? times: 1;
 var cp = require('child_process');
 for(var i = 0; i < times; i++) {
 	cp.fork(__dirname + '/test.js');
+	if(i % 100 == 9) console.log("%d Bots lunched !!", i + 1);
 }
