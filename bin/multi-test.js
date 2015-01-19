06ed91aebb6f33941ec3fc6d84622c78466481c0
Luphia Chang <luphia@outlook.com>
2015-01-19 09:57:46 +0800

    Tuning BOT

--- bin/multi-test.js
+++ bin/multi-test.js
@@ -5,10 +5,11 @@ times = times > 0? times: 100;
 
 var cp = require('child_process');
 var start;
+var myProcess = [];
 for(var i = 0; i < times; i++) {
 	if((i+1) % 100 == 1) start = new Date();
 
-	cp.fork(__dirname + '/test.js');
+	myProcess.push( cp.fork(__dirname + '/test.js') );
 
 	if((i+1) % 100 == 0) console.log("\x1b[32m%d Bots\x1b[0m lunched (per 100 in \x1b[32m%d\x1b[0m second)", i + 1, parseInt((new Date() - start) / 1000));
 }
