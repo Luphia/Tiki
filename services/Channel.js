9c913b58e511fa5e2c5c1677682b65643379ad0c
Luphia Chang <luphia@outlook.com>
2015-01-22 16:51:57 +0800

    Enhance memory usage

--- services/Channel.js
+++ services/Channel.js
@@ -3,8 +3,10 @@ var Channel = function(config) {
 };
 
 var lastUpdate = 0;
+var channelCtrl;
 
-Channel.prototype.init = function(config) {
+var ctrl = function(config) { this.init(); };
+ctrl.prototype.init = function(config) {
 	if(config) {
 		this.setConfig(config);
 	}
@@ -20,23 +22,10 @@ Channel.prototype.init = function(config) {
 	this.remainTickets = [];
 	this.point = 0;
 };
-Channel.prototype.setConfig = function(config) {
-	this.config = config;
-	this.io = require('socket.io').listen(this.config.get('server'), {
-	    'pingInterval': 3600000,
-	    'pingTimeout': 3600000
-	});
-};
-Channel.prototype.setSeller = function(seller) {
-	this.seller = seller;
-	//this.connection.remain = seller.remain();
-	this.remainTickets = seller.remainTickets;
-};
-
-Channel.prototype.getTickets = function() {
+ctrl.prototype.getTickets = function() {
 	return this.remainTickets;
 };
-Channel.prototype.getAreaTickets = function() {
+ctrl.prototype.getAreaTickets = function() {
 	var i = 0, rs = [];
 	var pick = 10;
 	if(pick > this.remainTickets.length) { return this.remainTickets; }
@@ -45,11 +34,11 @@ Channel.prototype.getAreaTickets = function() {
 
 	return this.remainTickets.slice(s, s + 10);
 };
-Channel.prototype.buyCode = function(code, client) {
+ctrl.prototype.buyCode = function(code, client) {
 	var rs = this.seller.sellByCode(code, client);
 	return rs;
 };
-Channel.prototype.buyType = function(type, number, client) {
+ctrl.prototype.buyType = function(type, number, client) {
 	var rs = [];
 	number = parseInt(number);
 	number = number > 0? number: 1;
@@ -60,7 +49,7 @@ Channel.prototype.buyType = function(type, number, client) {
 
 	return rs;
 };
-Channel.prototype.buyEvent = function(ev, number, client) {
+ctrl.prototype.buyEvent = function(ev, number, client) {
 	var rs = [];
 	number = parseInt(number);
 	number = number > 0? number: 1;
@@ -71,41 +60,59 @@ Channel.prototype.buyEvent = function(ev, number, client) {
 
 	return rs;
 };
-Channel.prototype.update = function() {
+ctrl.prototype.update = function() {
 	return true;
-	/*
-	var now = new Date() * 1;
-	var per = 10000;
-	if(now - lastUpdate > per) {
-
-		lastUpdate = now;
-		return true;
-	}
-	else {
-		return false;
-	}
-	*/
 };
-Channel.prototype.setOwner = function(client, customer) {
+ctrl.prototype.isFinish = function() {
+	if(this.remainTickets.length > 0) return false;
 
+	this.times++;
+	this.seller.loadTicket();
+	this.remainTickets = this.seller.remainTickets;
+
+	try {
+		this.io.emit('channel', {
+			"event": "reset",
+			"times": this.times
+		});
+	}
+	catch(e) {
+		console.log(e);
+	}
 };
 
+
+
+Channel.prototype.init = function(config) {
+	channelCtrl = new ctrl(config);
+};
+Channel.prototype.setConfig = function(config) {
+	this.config = config;
+	this.io = require('socket.io').listen(this.config.get('server'), {
+	    'pingInterval': 3600000,
+	    'pingTimeout': 3600000
+	});
+};
+Channel.prototype.setSeller = function(seller) {
+	channelCtrl.seller = seller;
+	//this.connection.remain = seller.remain();
+	channelCtrl.remainTickets = seller.remainTickets;
+};
 Channel.prototype.start = function() {
-	var self = this
-	,	io = this.io;
+	var io = this.io;
 
 	io.on('connection', function(socket) {
 		var customer = socket.client.id;
 
-		self.connection.current += 1;
-		self.connection.history += 1;
+		channelCtrl.connection.current += 1;
+		channelCtrl.connection.history += 1;
 
-		socket.emit('channel', {'event': 'enter', 'data': self.connection});
-		if(self.update()) { socket.broadcast.emit('channel', {'event': 'enter', 'data': self.connection}); }
+		socket.emit('channel', {'event': 'enter', 'data': channelCtrl.connection});
+		if(channelCtrl.update()) { socket.broadcast.emit('channel', {'event': 'enter', 'data': channelCtrl.connection}); }
 		socket.on('disconnect', function() {
-			self.connection.current -= 1;
+			channelCtrl.connection.current -= 1;
 
-			if(self.update()) { socket.broadcast.emit('channel', {'event': 'enter', 'data': self.connection}); }
+			if(channelCtrl.update()) { socket.broadcast.emit('channel', {'event': 'enter', 'data': channelCtrl.connection}); }
 		});
 
 		socket.on('channel', function(_event) {
@@ -117,8 +124,8 @@ Channel.prototype.start = function() {
 				case 'getTickets':
 					rs = {
 						"event": "getTickets",
-						"times": self.times,
-						"data": self.getTickets()
+						"times": channelCtrl.times,
+						"data": channelCtrl.getTickets()
 					};
 					socket.emit('channel', rs);
 					break;
@@ -126,8 +133,8 @@ Channel.prototype.start = function() {
 				case 'getAreaTickets':
 					rs = {
 						"event": "getTickets",
-						"times": self.times,
-						"data": self.getAreaTickets()
+						"times": channelCtrl.times,
+						"data": channelCtrl.getAreaTickets()
 					};
 					socket.emit('channel', rs);
 					break;
@@ -138,7 +145,7 @@ Channel.prototype.start = function() {
 
 					rs = {
 						"event": "buyCode",
-						"data": self.buyCode(code, client)
+						"data": channelCtrl.buyCode(code, client)
 					};
 
 					brs = {
@@ -158,7 +165,7 @@ Channel.prototype.start = function() {
 							io.emit('channel', brs);
 						}
 						catch(e) {}
-						self.isFinish();
+						channelCtrl.isFinish();
 					}
 					break;
 
@@ -169,7 +176,7 @@ Channel.prototype.start = function() {
 
 					rs = {
 						"event": "buyType",
-						"data": self.buyType(type, number, client)
+						"data": channelCtrl.buyType(type, number, client)
 					};
 
 					brs = {
@@ -191,7 +198,7 @@ Channel.prototype.start = function() {
 							io.emit('channel', brs);
 						}
 						catch(e) {}
-						self.isFinish();
+						channelCtrl.isFinish();
 					}
 					break;
 
@@ -202,7 +209,7 @@ Channel.prototype.start = function() {
 
 					rs = {
 						"event": "buyEvent",
-						"data": self.buyEvent(type, number, client)
+						"data": channelCtrl.buyEvent(type, number, client)
 					};
 
 					brs = {
@@ -224,30 +231,12 @@ Channel.prototype.start = function() {
 							io.emit('channel', brs);
 						}
 						catch(e) {}
-						self.isFinish();
+						channelCtrl.isFinish();
 					}
 					break;
 			}
 		});
 	});
-};
-
-Channel.prototype.isFinish = function() {
-	if(this.remainTickets.length > 0) return false;
-
-	this.times++;
-	this.seller.loadTicket();
-	this.remainTickets = this.seller.remainTickets;
-
-	try {
-		this.io.emit('channel', {
-			"event": "reset",
-			"times": this.times
-		});
-	}
-	catch(e) {
-		console.log(e);
-	}
 };
 
 module.exports = Channel;
